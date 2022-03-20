b598766d4aed76c520824f2f3cabc578ad0cefdf
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-03-20 20:08:26 +0530

    Update config.py

--- SiestaRobot/config.py
+++ SiestaRobot/config.py
@@ -17,17 +17,17 @@ class Config(object):
     API_ID = 123456  # integer value, dont use ""
     API_HASH = "awoo"
     TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 945137470  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "saint_foire"
-    SUPPORT_CHAT = "machinaxsupport"  # Your own group for support, do not add the @
+    OWNER_ID = 5009739318  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "sweetttu_1"
+    SUPPORT_CHAT = "Teddyrobot_support"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001180007354
+        -1001167536037
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001237968047
+        -1001167536037
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     ERROR_LOGS = (
-        -1001732618654
+        -1001167536037
     )  # Prints information Error
 
     # RECOMMENDED
