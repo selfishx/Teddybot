c838ca0af99aa8cf3838eb2a2394cf0da3ef7ff9
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-03-27 09:30:36 +0530

    Update animequotes.py

--- SiestaRobot/modules/animequotes.py
+++ SiestaRobot/modules/animequotes.py
@@ -29,7 +29,7 @@ def quotes(update: Update, context: CallbackContext):
     msg = f"<i>❝{quote}❞</i>\n\n<b>{character} from {anime}</b>"
     keyboard = InlineKeyboardMarkup([[
         InlineKeyboardButton(
-            text="Change🔁",
+            text="ᴄʜᴀɴɢᴇ🔁",
             callback_data="change_quote")]])
     message.reply_text(
         msg,
@@ -44,7 +44,7 @@ def change_quote(update: Update, context: CallbackContext):
     msg = f"<i>❝{quote}❞</i>\n\n<b>{character} from {anime}</b>"
     keyboard = InlineKeyboardMarkup([[
         InlineKeyboardButton(
-            text="Change🔁",
+            text="ᴄʜᴀɴɢᴇ🔁",
             callback_data="quote_change")]])
     message.edit_text(msg, reply_markup=keyboard,
                       parse_mode=ParseMode.HTML)
