e1e95ad539b3004874ad937cbd0270b753fac8f8
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-03-27 09:15:05 +0530

    Update google.py

--- SiestaRobot/modules/google.py
+++ SiestaRobot/modules/google.py
@@ -10,7 +10,7 @@ from urllib.parse import urlencode
 import requests
 from bs4 import BeautifulSoup
 from PIL import Image
-from search_engine_parser import GoogleSearch
+from search_engine_parser.core.engines.google import Search as GoogleSearch
 
 import bs4
 import html2text
@@ -24,6 +24,7 @@ from SiestaRobot import *
 
 from SiestaRobot.events import register
 from SiestaRobot import telethn as tbot
+from SiestaRobot.modules.language import gs
 
 opener = urllib.request.build_opener()
 useragent = "Mozilla/5.0 (Linux; Android 9; SM-G960F Build/PPR1.180610.011; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/74.0.3729.157 Mobile Safari/537.36"
@@ -35,7 +36,7 @@ async def _(event):
     if event.fwd_from:
         return
 
-    webevent = await event.reply("searching.....")
+    webevent = await event.reply("searching........")
     match = event.pattern_match.group(1)
     page = re.findall(r"page=\d+", match)
     try:
@@ -268,7 +269,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Emiko <==="
+        app_details += "\n\n===> Siesta <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
@@ -276,17 +277,8 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "Search"
+def helps(chat):
+    return gs(chat, "search_help")
 
-__help__ = """
-❂ /google <query>*:* Perform a google search
-❂ /image <query>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
-❂ /app <appname>*:* Searches for an app in Play Store and returns its details.
-❂ /reverse: Does a reverse image search of the media which it was replied to.
-❂ /gps <location>*:* Get gps location.
-❂ /github <username>*:* Get information about a GitHub user.
-❂ /country <country name>*:* Gathering info about given country
-❂ /imdb <Movie name>*:* Get full info about a movie with imdb.com
-❂ Emiko <query>*:* Emiko answers the query
-  💡Ex: `Teddy where is Japan?`
-"""
+
+__mod_name__ = "Search"
