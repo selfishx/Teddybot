332a1aa47d7879e58cae3c46b9d75721e50d8007
Suru <100470320+SuruXmanager@users.noreply.github.com>
2022-03-27 09:22:53 +0530

    Update shippering.py

--- SiestaRobot/modules/shippering.py
+++ SiestaRobot/modules/shippering.py
@@ -65,7 +65,7 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
             c2_id = int(is_selected["c2_id"])
             c1_name = (await app.get_users(c1_id)).first_name
             c2_name = (await app.get_users(c2_id)).first_name
-            couple_selection_message = f"""Couple of the day:
+            couple_selection_message = f"""**Couple of the day:**
 [{c1_name}](tg://openmessage?user_id={c1_id}) + [{c2_name}](tg://openmessage?user_id={c2_id}) = ❤️
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
             await app.send_message(message.chat.id, text=couple_selection_message)
